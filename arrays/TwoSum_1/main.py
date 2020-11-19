b232791d040adb65915a6fa4c6e564cc13c01f38
dankos914 <kossenko.daniel@gmail.com>
2020-11-19 18:20:24 +0200

    1 added

---
+++ arrays/TwoSum_1/main.py
@@ -0,0 +1,16 @@
+# Не решено
+
+from typing import List
+class Solution:
+    def twoSum(self,nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, num in enumerate(nums):
+            difference = target - num #
+            if difference in d:
+                return d[difference], i
+            else:
+                d[num] = i
+
+solution = Solution()
+print(solution.twoSum([2,5,5,11],10))
+#[2,7,11,15, 9]
