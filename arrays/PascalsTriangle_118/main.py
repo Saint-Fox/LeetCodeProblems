01d6e69cf218003645db52ef51c52d36bfcb72ae
Saint-Fox <daniil.k@ukr.net>
2020-11-20 21:20:02 +0200

    35 done

--- arrays/PascalsTriangle_118/main.py
+++ arrays/PascalsTriangle_118/main.py
@@ -17,8 +17,19 @@ class Solution:
             t_l.append(l)
         return t_l
 
+    def updated_solution(self, numRows: int) -> List[List[int]]:
+        triangle = []
+        for num_row in range(numRows):
+            row = [None for _ in range(num_row + 1)]
+            row[0], row[-1] = 1, 1
+            # print(row)
+            for j in range(1, len(row)-1):
+                print(j)
+                row[j] = triangle[num_row - 1][j-1] + triangle[num_row - 1][j]
+            triangle.append(row)
+        return triangle
 
 solution = Solution()
-print(solution.generate(5))
-# print(fact(5))
+print(solution.updated_solution(5))
+# print([_ for _ in range(5)])
 # n! = n*(n-1)*(n-2)*...*2*1
--- arrays/SearchInsertPosition_35/main.py
+++ arrays/SearchInsertPosition_35/main.py
@@ -5,7 +5,17 @@ from typing import List
 
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        print("Not done")
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            av = int((low+high)/2)
+            if nums[av] == target:
+                return av
+            elif target > nums[av]:
+                low = av + 1
+            else:
+                high = av - 1
+        return low
 
 solution = Solution()
-print(solution.searchInsert([2,3,4,8,10], 0))
+print(solution.searchInsert([1,3,5,6], 5))
